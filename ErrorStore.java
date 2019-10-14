7c9f40f0f35b6448f6e4fdcc2952263a2767ee70
msgtovp <msgtovp@gmail.com>
2019-10-14 15:29:51 +0530

    Create ErrorStore.java

---
+++ ErrorStore.java
@@ -0,0 +1,17 @@
+package com.blogger.rest.store;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ErrorStore {
+	
+	static public Map getErrorResponse(Integer status, String message) {
+		Map<String, String> resp = new HashMap<String, String>();
+		
+		resp.put("errorCode", String.valueOf(status));
+		resp.put("errorMessage", message);
+		
+		return resp;
+	}
+
+}
